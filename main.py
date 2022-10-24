26a044e20ec571eee44d888b7b55cec2ebd77292
ASANOVV <mirbek020295@gmail.com>
2022-10-24 15:53:46 +0300

    homework 5

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import asyncio
 from aiogram.utils import executor
 from config import bot, dp
 import logging
-from handlers import admin, callback, client, extra, fsm_mentor
+from handlers import admin, callback, client, extra, fsm_mentor, notifications
 from database.bot_db import sql_create
 
 
 async def on_startup(_):
+    asyncio.create_task(notifications.scheduler())
     sql_create()
 
 
+notifications.register_handlers_notification(dp)
 callback.register_handlers_callback(dp)
 client.register_handlers_client(dp)
 fsm_mentor.register_handlers_fsm(dp)
